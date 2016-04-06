8e8824e64496e0e32cdbf8a810dccb4639e3540f
scoobaroo <scoobaroo415@gmail.com>
2016-04-05 21:58:08 -0700

    sprint 5 completed, spring 6 1/2 done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,15 +25,114 @@ $(document).ready(function() {
     $(this).trigger("reset");
   });
 
-
   // catch and handle the click on an add song button
   $('#albums').on('click', '.add-song', handleAddSongClick);
 
   // save song modal save button
   $('#saveSong').on('click', handleNewSongSubmit);
+var albumId;
+  $('#albums').on('click', '.edit-song', function editSong(e){
+      e.preventDefault();
+      albumId = $(this).closest('.album').data('album-id');
+      var $modal = $('#editSongsModal');
+      var $songNameField = $modal.find('#editedSongName');
+      var $trackNumberField = $modal.find('#editedTrackNumber');
+      var dataToPost = {
+        name: $songNameField.val(),
+        trackNumber: $trackNumberField.val()
+      };
+      $.ajax({
+        method: 'GET',
+        url: '/api/albums/'+albumId+'/songs',
+        success: getSongSuccess,
+        error: getSongError
+      });
+      console.log('retrieved songName:', songName, ' and trackNumber:', trackNumber, ' for album w/ id: ', albumId);
+      // POST to SERVER
+      $('#editSongsModal').modal('show');
+  });
+$('#editSongsModal').on('click', '.delete-song', function editSong(e){
+      e.preventDefault();
+      var songId = $(this).closest('.song-form').data('song-id');
+      console.log(songId);
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/albums/'+albumId+'/songs/'+songId,
+      success: deleteSongSuccess,
+      error: deleteSongError
+    });
+});
+function deleteSongSuccess(json){
+  console.log('deleted Song!');
+}
+function deleteSongError(json){
+  console.log('delete song error!');
+}
+function getSongSuccess(json){
+  var songListHtml = $('#songlist').html();
+  var songTemplate = Handlebars.compile(songListHtml);
+  var songhtml = songTemplate({songsList: json});
+  console.log(json);
+  $('#editSongsModalBody').append(songhtml);
+}
+
+  $('#albums').on('click', '.edit-album', function handleDeleteAlbum(e){
+    var albumId = $(this).closest('.album').data('album-id');
+    $('.edit-album').html('<button class="btn btn-danger edit" id="saveChanges">Save Changes</button>');
+    $('.album-name').html('<input type=text id="album-name-edited">');
+    $('.artist-name').html('<input type=text id="artist-name-edited">');
+    $('.album-releaseDate').html('<input type=text id="releaseDate-edited">');
+    $('.songs').html('<input type=text id="songs-edited">');
+  });
+
+  $('#albums').on('click', '#saveChanges', function saveChanges(e){
+    e.preventDefault();
+    var albumId = $(this).closest('.album').data('album-id');
+    var editedArtist = $('#artist-name-edited').val();
+    var editedAlbum = $('#album-name-edited').val();
+    var editedRD = $('#releaseDate-edited').val();
+    var editedSongs = $('#songs-edited').val();
+    $.ajax({
+      method: 'PUT',
+      url: 'api/albums/'+albumId,
+      data: {name: editedAlbum, artistName: editedArtist, releaseDate: editedRD, songs: editedSongs},
+      success: onEditSuccess,
+      error: onEditError
+    });
+  });
+
+
+  $('#albums').on('click', '.delete-album', function handleDeleteAlbum(e){
+    console.log('delete album clicked');
+    console.log($(this).closest('.album').data('album-id'));
+    var albumId = $(this).closest('.album').data('album-id');
+    $.ajax({
+      method: 'DELETE',
+      url: '/api/albums/'+albumId,
+      success: deleteAlbumSuccess,
+      error: deleteAlbumError
+    });
+  });
 });
 
-
+function onEditSuccess(album){
+  console.log("edit success!");
+  console.log(album.name);
+  $('.album-name').text(album.name);
+  $('.artist-name').text(album.artistName);
+  $('.album-releaseDate').text(album.releaseDate);
+  $('.songs').text(album.songs);
+}
+function onEditError(json){
+  console.log('edit Error');
+  console.log(json);
+}
+function deleteAlbumSuccess(json){
+  console.log('deleted Album!');
+}
+function deleteAlbumError(json){
+  console.log('error'+json);
+}
 
 // this function takes a single album and renders it to the page
 function renderAlbum(album) {
@@ -44,6 +143,9 @@ function renderAlbum(album) {
   $('#albums').prepend(html);
 }
 
+function getSongError(json){
+  console.log(json);
+}
 // when the add song button is clicked, display the modal
 function handleAddSongClick(e) {
   console.log('add-song clicked!');
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 // SERVER-SIDE JAVASCRIPT
-
 //require express in our app
 var express = require('express');
 // generate a new express app and call it 'app'
 var app = express();
 var bodyParser = require('body-parser');
-
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-
 // We'll serve jQuery and bootstrap from a local bower cache avoiding CDNs
 // We're placing these under /vendor to differentiate them from our own assets
 app.use('/vendor', express.static(__dirname + '/bower_components'));
-
 var controllers = require('./controllers');
-
-
+var db = require('./models');
 /**********
  * ROUTES *
  **********/
@@ -35,12 +30,59 @@ app.get('/', function homepage (req, res) {
  */
 
 app.get('/api', controllers.api.index);
-
 app.get('/api/albums', controllers.albums.index);
 app.get('/api/albums/:albumId', controllers.albums.show);
 app.post('/api/albums', controllers.albums.create);
 app.post('/api/albums/:albumId/songs', controllers.albumsSongs.create);
 
+app.delete('/api/albums/:albumId/songs/:songId', function deleteSong(req,res){
+  db.Album.findById(req.params.albumId, function(err,foundAlbum) {
+      foundAlbum.Song.id(req.params.songId).remove();
+    });
+});
+
+app.get('/api/albums/:albumId/songs', function getSongs(req,res){
+    var albumId = req.params.albumId;
+    db.Album.findById(req.params.albumId, function(err, foundAlbum) {
+      console.log(foundAlbum);
+      var songList = foundAlbum.songs;
+      console.log(songList); // dangerous, in a real app we'd validate the incoming data
+      res.json(songList);  // responding with just the song, some APIs may respond with the parent object (Album in this case)
+    });
+});
+
+app.put('/api/albums/:albumId', function editAlbum(req,res){
+  console.log(req.params);
+  var albumId = req.params.albumId;
+  var editedAlbumName = req.body.name;
+  var editedArtistName = req.body.artistName;
+  var editedRD = req.body.releaseDate;
+  var editedSongs = req.body.songs;
+  console.log(albumId, editedAlbumName, editedArtistName, editedRD, editedSongs);
+  db.Album.findByIdAndUpdate(albumId, {name:editedAlbumName,
+                                      artistName:editedArtistName,
+                                      releaseDate:editedRD,
+                                      songs:editedSongs},  {new: true}, function(err, datares){
+    if(err){
+      console.log(err);
+      res.status(500).json(err);
+      }
+    else
+      console.log(datares);
+      res.status(200).json(datares);
+    });
+});
+
+app.delete('/api/albums/:albumId' ,function deleteAlbum (req,res){
+  var albumId = req.params.albumId;
+  db.Album.findByIdAndRemove(albumId, function(err, datares){
+    if(err){
+      console.log(err);
+      }
+    else
+      console.log(datares);
+    });
+});
 
 /**********
  * SERVER *
